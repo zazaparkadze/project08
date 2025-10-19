e3a382b16142a1de80c95e8d65bea1fe2815726b
Zaza Parkadze <zaza.parkadze@gmail.com>
2025-10-19 16:54:52 +0300

    environment

---
+++ src/app/users/[username]/[id]/loading.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function loading() {
+  return <div className="pt-10 pl-10 text-3xl text-amber-600">loading...</div>;
+}
--- src/lib/connectDB.tsx
+++ src/lib/connectDB.tsx
@@ -13,17 +13,13 @@ export default async function connectDB() {
 
   try {
     await mongoose
-      .connect(
-        process.env.DATA_BASE_URI ||
-          "mongodb+srv://zaza:1234test@cluster0.crzqo.mongodb.net/mongoData?retryWrites=true&w=majority&appName=Cluster0",
-        {
-          maxPoolSize: 10,
-          minPoolSize: 1,
-          connectTimeoutMS: 15000,
-          serverSelectionTimeoutMS: 11000,
-          socketTimeoutMS: 180000,
-        }
-      )
+      .connect(process.env.DATA_BASE_URI as string, {
+        maxPoolSize: 10,
+        minPoolSize: 1,
+        connectTimeoutMS: 15000,
+        serverSelectionTimeoutMS: 11000,
+        socketTimeoutMS: 180000,
+      })
       .then(() => console.log("connected to mongoDB"));
   } catch (error) {
     console.log(error);
