de1503ab23199250ea86c9026e1a9e66faeed670
Zaza Parkadze <zaza.parkadze@gmail.com>
2025-11-02 18:01:01 +0200

    cors.

--- src/app/users/[username]/[id]/hello/page.tsx
+++ src/app/users/[username]/[id]/hello/page.tsx
@@ -1,6 +1,10 @@
 import HelloPage from "./component/HelloPage";
 
 export default async function page() {
-  const url = "http://localhost:3000/api/hello";
+  const url =
+    process.env.NODE_ENV === "production"
+      ? "https://project08-bay.vercel.app/api/hello"
+      : "http://localhost:3000/api/hello";
+
   return <HelloPage url={url} />;
 }
